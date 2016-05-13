cb8a1a13e223ebaf60066a21def78cd849ebed63
Tim Oram <mitmaro@gmail.com>
2016-05-13 12:00:11 -0230

    Initial project commit

---
+++ test/lib/rules/strict-newline.js
@@ -0,0 +1,109 @@
+'use strict';
+
+var rule = require('../../../lib/rules/strict-newline');
+var RuleTester = require('eslint').RuleTester;
+
+var parserOptions = {};
+
+var ruleTester = new RuleTester();
+ruleTester.run('strict-newline', rule, {
+	valid: [
+		{
+			code: [
+				'"use strict"',
+				'',
+				'var a;'
+			].join('\n'),
+			parserOptions: parserOptions
+		},
+		{
+			code: [
+				'"use strict"',
+				'',
+				'var a;'
+			].join('\n'),
+			parserOptions: parserOptions
+		},
+		{
+			code: [
+				'"use strict"',
+				''
+			].join('\n'),
+			parserOptions: parserOptions
+		},
+		{
+			code: [
+				'// comment',
+				'"use strict"',
+				''
+			].join('\n'),
+			parserOptions: parserOptions
+		},
+		{
+			code: [
+				'/* comment */',
+				'"use strict"',
+				''
+			].join('\n'),
+			parserOptions: parserOptions
+		},
+		{
+			code: [
+				'/*',
+				'comment',
+				'*/',
+				'"use strict"',
+				''
+			].join('\n'),
+			parserOptions: parserOptions
+		},
+		{
+			code: '',
+			parserOptions: parserOptions
+		}
+	],
+
+	invalid: [
+		{
+			code: [
+				'"use strict"',
+				'var a;'
+			].join('\n'),
+			parserOptions: parserOptions,
+			errors: [{message: 'Expected newline after \'use strict\''}]
+		},
+		{
+			code: [
+				'"use strict";var a;'
+			].join('\n'),
+			parserOptions: parserOptions,
+			errors: [{message: 'Expected newline after \'use strict\''}]
+		},
+		{
+			code: [
+				'"use strict"',
+				'// comment'
+			].join('\n'),
+			parserOptions: parserOptions,
+			errors: [{message: 'Expected newline after \'use strict\''}]
+		},
+		{
+			code: [
+				'"use strict"',
+				'/* comment */'
+			].join('\n'),
+			parserOptions: parserOptions,
+			errors: [{message: 'Expected newline after \'use strict\''}]
+		},
+		{
+			code: [
+				'"use strict"',
+				'/*',
+				'comment',
+				'*/'
+			].join('\n'),
+			parserOptions: parserOptions,
+			errors: [{message: 'Expected newline after \'use strict\''}]
+		}
+	]
+});
